76556bdd842fab416a50904d2f1f04b4296448bc
edwarddubi <59847429+edwarddubi@users.noreply.github.com>
2020-11-30 20:22:26 -0500

    bot done, updated readme

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-# zSteps 
+# zSteps (Discord Bot)
 **Take steps before others to attain an object**
-Imagine people having the same thing in common; however, this thing can only belong to one person. This game was made to turn these people into players. Each player would take random steps by competing with other rivals. Before the game begins, the players would decide the total steps needed to take to get to the destination, where the object is located as well as the object name. The winner is determined if a player has accumulated the exact steps specified by all the players.
 
+Participants would decide on an object they would like to own for themselves. They become players and would compete against themselves. Each player would take random steps between 0-10 (excluding 0 and 10). Before the game begins, the players would decide on the total number of steps needed to take to get to the destination, where the object is located at as well as the object's name. The winner is determined if a player has accumulated the exact or more steps specified by all the players. Tie players would need a rematch (start a new game) until one person wins.
+### Note
+The object would be given to the winner at the end of the game.
+
+## Rules of the zSteps Game
+ - Players should type "play" to be added to the game.
+ - Anyone can type "start, n, x" when all participating players have joined. The "n" is the number of steps to be taken by each player to reach the location the object is located at; also, the "x" is the name of the object to be attained by a player. For example, "start, 100, crown".
+ - If no steps are specified, the default steps to be taken is a 100. Also, if no object is specified, the default object would be a Crown.
+ - To start playing, type "takeStep, y" where "y" represents your previous step(s). In the beginning, since no one has a step count, each player would start with a 0 e.g "takeStep, 0".
+ - Players would be penalized by taking 5 steps back if they do not obey rule no.4.
+ - The steps made by a player would be a number between 0-10; thus, players must type as fast as they can.
+ - This is not a turn based game, but measures how fast a player can gain steps to reach their destination.- The winner is determined when a player has the exact or more "n" steps.
+
+## Init Game on Discord
+ - **init zsteps**
+## Start Game on Discord
+ - **start, 100, pizza**, where 100 is the step count and the pizza is the object to be attained by one player out of all the players
+
+ ## Quit?
+ - Type **quit** or **exit**
 
 
 ## Dependencies
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def on_message(message):
             return await channel.send("You can't start a game that has already been started")
         else:
             msg = 'Thanks for waking me up from my slumber.\nRules for the game are:\n1. Players should type "play" to be added to the game.\n2. Anyone can type "start, n, x" when all participating players have joined. The "n" is the number of steps to be taken by each player to reach the location the object is located at; also, the "x" is the name of the object to be attained by a player. For example, "start, 100, crown".\n3. If no steps are specified, the default steps to be taken is a 100. Also, if no object is specified, the default object would be a Crown.\n4. To start playing, type "takeStep, y" where "y" represents your previous step(s). In the beginning, since no one has a step count, each player would start with a 0 e.g "takeStep, 0".\n5. Players would be penalized by taking 5 steps back if they do not obey rule no.4 \n6. The steps made by a player would be a number between 0-10; thus, players must type as fast as they can.\n6. This is not a turn based game, but how fast a player can gain steps to reach their destination.\n7. The winner is determined when a player has the exact or more "n" steps.\n8. Play away!! And test how good your RNG is...haha'
-            msg = "Thanks for waking me up from my slumber.\nRules for the game are:\n"
+            #msg = "Thanks for waking me up from my slumber.\nRules for the game are:\n"
             steps.is_awake = True
             return await channel.send(msg)
 
@@ -45,7 +45,7 @@ async def on_message(message):
                 for player in steps.players:
                     if player["total_steps"] >= steps.step_count:
                         end_game = True
-                        break
+                        steps.winners.append(player)
                 for player in steps.players:
                     if player["id"] == id:
                         player["total_steps"] += rand_num
@@ -69,20 +69,27 @@ async def on_message(message):
                                     player["total_steps"] = 0
                                 penalized = True
                             pass
-                        msg = '{0} took {1} step(s) towards the goal\nTotal accumulated steps: {2}\n'.format(player["name"], rand_num, player["total_steps"])
+                        msg = '{0} took {1} step(s) towards the {2}\nTotal accumulated steps: {3}\n'.format(player["name"], rand_num, steps.price, player["total_steps"])
                         if penalized:
-                            msg+="You've been penalized by taking 5 steps back. To know why please read the rules at the beginning of the game."
+                            msg+="You've been penalized by taking 5 steps back. To know why, please read the rules at the beginning of the game."
                         
                         await channel.send(msg)
                         break
                 
                 if end_game:
-                    steps.winners = sorted(steps.players, key=lambda player : player["total_steps"])
+                    steps.players = sorted(steps.players, key=lambda p : p['total_steps'])
+                    steps.players.reverse()
                     c = 1
-                    msg = "Let's see who got the {0}\n".format(steps.price)
-                    for winner in steps.winners:
-                        msg+="{0}. {1}: {2}".format(c, winner["name"], winner["total_steps"])
-                        
+                    msg = "Let's see who got the {0}...\n".format(steps.price)
+                    for player in steps.players:
+                        msg+="{0}. {1}: {2}.\n".format(c, player["name"], player["total_steps"])
+                        c+=1
+                    
+                    if len(steps.winners) > 1:
+                        msg+='There was a tie! Since more than one person won the game, there is no winner. The winners should start a new game and have a rematch until one person wins.'
+                    else:
+                        player = steps.players[0]
+                        msg+='<@{0}> won the game; thus, they deserve the {1}!'.format(player['id'], steps.price)
                     steps.reset()
                         
                     return await channel.send(msg)
